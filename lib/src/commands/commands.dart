92eb5067f98dc221fac6a1c5b4543549544bfb78
Abdulhaq Emhemmed <abdulhaq.emhemmed@gmail.com>
2025-04-13 23:30:49 +0200

    fix

--- lib/src/commands/commands.dart
+++ lib/src/commands/commands.dart
@@ -1,5 +1,4 @@
 export './bloc/command.bloc.dart';
-export './views/command_view.dart';
 export './views/command_view2.dart';
 export './views/transparent_command_widget.dart';
 export './bloc/null_error_handler.dart';
