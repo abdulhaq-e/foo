2ae0edefb1bf9dec3c70b3b6c435e0fe55037bb1
Abdulhaq Emhemmed <abdulhaq.emhemmed@gmail.com>
2025-05-04 12:07:44 +0200

    delete forms v1, a disaster, wasted 2 hours of debugging

--- lib/src/forms/bloc/form.events.dart
+++
@@ -1,9 +0,0 @@
-part of 'form.bloc.dart';
-
-abstract class FooFormEvent {}
-
-class FooFormDataLoaded extends FooFormEvent {}
-
-class FooFormDataReloaded extends FooFormEvent {}
-
-class FooFormSubmitted extends FooFormEvent {}
--- lib/src/forms/forms.dart
+++
@@ -1,5 +0,0 @@
-export 'bloc/form.bloc.dart';
-export 'dependencies.dart';
-export 'views/form_builder.dart';
-export 'views/form.dart';
-export 'views/form_components_helper.dart';
