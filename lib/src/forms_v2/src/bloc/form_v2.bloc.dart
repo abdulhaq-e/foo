c52608178a3406d72a2dc9ea1b2cb2065fb126a5
Abdulhaq Emhemmed <abdulhaq.emhemmed@gmail.com>
2025-11-07 00:25:51 +0200

    field updated

--- lib/src/forms_v2/src/bloc/form_v2.bloc.dart
+++ lib/src/forms_v2/src/bloc/form_v2.bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:foo/core.dart';
@@ -15,39 +14,39 @@ typedef OnValueChange<F> = FutureOr<void> Function(F);
 
 class FooFormV2Bloc<Data, Form extends HasFormV2Group>
     extends Bloc<FooFormV2Event, FooFormV2State<Data, Form>> {
-  FormV2Builder<Form> _formBuilder;
-  QueryHandling<Form, Data> _dataFetcher;
-  CommandHandling<Form, void> _submitCommandHandler;
-  bool _shouldListenToValueChanges;
-  OnValueChange<Form>? _onValueChange;
-
-  StreamSubscription<dynamic>? _subscription;
-
   FooFormV2Bloc({
     required FormV2Builder<Form> formBuilder,
     required QueryHandling<Form, Data> dataFetcher,
     required CommandHandling<Form, void> submitCommandHandler,
     bool shouldListenToValueChanges = false,
     OnValueChange<Form>? onValueChange,
-  })  : _formBuilder = formBuilder,
-        _dataFetcher = dataFetcher,
-        _submitCommandHandler = submitCommandHandler,
-        _shouldListenToValueChanges = shouldListenToValueChanges,
-        _onValueChange = onValueChange,
-        super(FooFormV2State.initial()) {
-    on<FooFormV2DataLoaded>((event, emit) => _loadData(event, emit));
-    on<FooFormV2DataReloaded>((event, emit) => _reloadData(event, emit));
-    on<FooFormV2Submitted>((event, emit) => _submit(event, emit));
+  }) : _formBuilder = formBuilder,
+       _dataFetcher = dataFetcher,
+       _submitCommandHandler = submitCommandHandler,
+       _shouldListenToValueChanges = shouldListenToValueChanges,
+       _onValueChange = onValueChange,
+       super(FooFormV2State.initial()) {
+    on<FooFormV2DataLoaded>(_loadData);
+    on<FooFormV2DataReloaded>(_reloadData);
+    on<FooFormV2Submitted>(_submit);
+    on<FooFormV2FieldUpdated>(_fieldUpdated);
   }
+  final FormV2Builder<Form> _formBuilder;
+  final QueryHandling<Form, Data> _dataFetcher;
+  final CommandHandling<Form, void> _submitCommandHandler;
+  final bool _shouldListenToValueChanges;
+  final OnValueChange<Form>? _onValueChange;
 
-  void _loadData(event, emit) async {
+  StreamSubscription<dynamic>? _subscription;
+
+  Future<void> _loadData(event, emit) async {
     emit(FooFormV2State<Data, Form>.loading());
     try {
-      var form = _formBuilder();
-      var formData = await this._dataFetcher(form);
+      final form = _formBuilder();
+      final formData = await _dataFetcher(form);
       if (_shouldListenToValueChanges && _onValueChange != null) {
         _subscription = form.formGroup.valueChanges.listen((_) async {
-          await _onValueChange!(form);
+          await _onValueChange(form);
         });
       }
       emit(FooFormV2State<Data, Form>.loaded(formData: formData, form: form));
@@ -56,32 +55,52 @@ class FooFormV2Bloc<Data, Form extends HasFormV2Group>
     }
   }
 
-  void _reloadData(event, emit) async {
+  Future<void> _reloadData(event, emit) async {
     // Can be used for situations when a drop menu selection will cause another
     // drop menu data to be loaded.
-    if (!(state is FooFormV2Loaded<Data, Form>)) {
+    if (state is! FooFormV2Loaded<Data, Form>) {
       return;
     }
-    var newState = state as FooFormV2Loaded<Data, Form>;
-    emit(newState.copyWith(
-        formDataUpdateStatus: FooFormV2DataUpdateStatus.updating));
+    final newState = state as FooFormV2Loaded<Data, Form>;
+    emit(
+      newState.copyWith(
+        formDataUpdateStatus: FooFormV2DataUpdateStatus.updating,
+      ),
+    );
     try {
-      var formData = await this._dataFetcher(newState.form);
-      emit(newState.copyWith(
+      final formData = await _dataFetcher(newState.form);
+      emit(
+        newState.copyWith(
           formData: formData,
-          formDataUpdateStatus: FooFormV2DataUpdateStatus.updating));
+          formDataUpdateStatus: FooFormV2DataUpdateStatus.updating,
+        ),
+      );
     } catch (_) {
-      emit(newState.copyWith(
-          formDataUpdateStatus: FooFormV2DataUpdateStatus.failed));
+      emit(
+        newState.copyWith(
+          formDataUpdateStatus: FooFormV2DataUpdateStatus.failed,
+        ),
+      );
     }
   }
 
-  void _submit(event, emit) async {
-    if (!(state is FooFormV2Loaded<Data, Form>)) {
+  Future<void> _submit(event, emit) async {
+    if (state is! FooFormV2Loaded<Data, Form>) {
       return;
     }
-    var newState = state as FooFormV2Loaded<Data, Form>;
+    final newState = state as FooFormV2Loaded<Data, Form>;
 
     await _submitCommandHandler(newState.form);
+  }
+
+  Future<void> _fieldUpdated(FooFormV2FieldUpdated event, emit) async {
+    if (state is! FooFormV2Loaded<Data, Form>) {
+      return;
+    }
+    final newState = state as FooFormV2Loaded<Data, Form>;
+
+    newState.form.formGroup.controls[event.formControl]?.updateValue(
+      event.value,
+    );
   }
 }
--- lib/src/forms_v2/src/bloc/form_v2.events.dart
+++ lib/src/forms_v2/src/bloc/form_v2.events.dart
@@ -7,3 +7,9 @@ class FooFormV2DataLoaded extends FooFormV2Event {}
 class FooFormV2DataReloaded extends FooFormV2Event {}
 
 class FooFormV2Submitted extends FooFormV2Event {}
+
+class FooFormV2FieldUpdated extends FooFormV2Event {
+  FooFormV2FieldUpdated({required this.formControl, required this.value});
+  final String formControl;
+  final dynamic value;
+}
