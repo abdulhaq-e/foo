52b329329793e75122a9180f26a191a42f9a28ba
Abdulhaq Emhemmed <abdulhaq.emhemmed@gmail.com>
2025-12-31 21:45:49 +0200

    export boostrap context

--- lib/src/app-core/src/bootstrap/bootstrap.dart
+++ lib/src/app-core/src/bootstrap/bootstrap.dart
@@ -1,5 +1,6 @@
 export 'src/app_bootstrapper.dart';
 export 'src/auth_bootstrapper.dart';
+export 'src/bootrap_context.dart';
 export 'src/feature_registration.dart';
 export 'src/feature_registry.dart';
 export 'src/saas_tenant_bootstrapper.dart';
